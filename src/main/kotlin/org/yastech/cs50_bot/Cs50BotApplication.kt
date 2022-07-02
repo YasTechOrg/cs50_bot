3316b552253296e9ee16460131c7a067d5900c0d
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-07-02 15:57:05 +0430

    FIX: Rebase /generate structure /generate edit data and cansel actions

--- src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
+++ src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
@@ -1,7 +1,11 @@
 package org.yastech.cs50_bot
 
 import com.elbekd.bot.Bot
+import com.elbekd.bot.feature.chain.chain
+import com.elbekd.bot.feature.chain.jumpTo
+import com.elbekd.bot.feature.chain.jumpToAndFire
 import com.elbekd.bot.model.toChatId
+import com.elbekd.bot.types.*
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
@@ -42,9 +46,46 @@ fun main(args: Array<String>)
 	}
 
 	// Generate Command
-	bot.onCommand("/generate") { (msg, _) ->
-		bot.sendMessage(msg.chat.id.toChatId(), "On Development!")
-	}
+	var nameFa = ""
+	var nameEn: String
+	var msgID = 0L
+	bot.chain("/generate")
+	{
+		bot.jumpToAndFire("run", it)
+	}.then("run"){
+		bot.sendMessage(it.chat.id.toChatId(), "لطفا نام و نام خانوادگی خود را به فارسی وارد کنید!")
+	}.then{
+		nameFa = "${it.text}"
+		bot.sendMessage(it.chat.id.toChatId(), "ازت ممنونم ${it.text}.\n" +
+				"لطفا نام و نام خانوادگی خود را به انگلیسی وارد کنید!")
+	}.then {
+		nameEn ="${it.text}"
+		bot.sendMessage(it.chat.id.toChatId(), "نام فارسی : $nameFa\n" +
+				"نام انگلیسی : $nameEn\n" +
+				"آیا اطلاعات وارد شده درست است ؟", replyMarkup = ReplyKeyboardMarkup( keyboard = listOf(
+			listOf(
+				KeyboardButton("لغو"),
+				KeyboardButton("ویرایش اطلاعات"),
+			),
+			listOf(
+				KeyboardButton("ساختن تصویر نوشته جدید!")
+			)
+		),
+		oneTimeKeyboard = true)).let { msg ->
+			msgID = msg.messageId
+		}
+	}.then {
+		when(it.text)
+		{
+			"لغو" ->
+			{
+				bot.sendMessage(it.chat.id.toChatId(), "عملیات با موفقیت لغو شد!")
+				bot.deleteMessage(it.chat.id.toChatId(), msgID)
+			}
+			"ویرایش اطلاعات" -> bot.jumpToAndFire("run", it)
+			"ساختن تصویر نوشته جدید!" -> bot.sendMessage(it.chat.id.toChatId(), "ساحته شد !")
+		}
+	}.build()
 
 	// Start Bot
 	bot.start()
