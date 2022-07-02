09aed7c579f134970316e431bcb4354bb19e3674
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-07-03 02:29:26 +0430

    FEAT: Generate Full Process FIT: A Bunch of BUG

--- src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
+++ src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
@@ -2,12 +2,23 @@ package org.yastech.cs50_bot
 
 import com.elbekd.bot.Bot
 import com.elbekd.bot.feature.chain.chain
-import com.elbekd.bot.feature.chain.jumpTo
 import com.elbekd.bot.feature.chain.jumpToAndFire
 import com.elbekd.bot.model.toChatId
-import com.elbekd.bot.types.*
+import com.elbekd.bot.types.KeyboardButton
+import com.elbekd.bot.types.ReplyKeyboardMarkup
+import com.elbekd.bot.util.SendingByteArray
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.util.ResourceUtils
+import java.awt.*
+import java.awt.font.TextAttribute
+import java.awt.geom.RoundRectangle2D
+import java.awt.image.BufferedImage
+import java.io.ByteArrayOutputStream
+import java.net.URL
+import java.text.AttributedString
+import javax.imageio.ImageIO
+
 
 @SpringBootApplication
 class Cs50BotApplication
@@ -47,7 +58,7 @@ fun main(args: Array<String>)
 
 	// Generate Command
 	var nameFa = ""
-	var nameEn: String
+	var nameEn = ""
 	var msgID = 0L
 	bot.chain("/generate")
 	{
@@ -56,10 +67,10 @@ fun main(args: Array<String>)
 		bot.sendMessage(it.chat.id.toChatId(), "لطفا نام و نام خانوادگی خود را به فارسی وارد کنید!")
 	}.then{
 		nameFa = "${it.text}"
-		bot.sendMessage(it.chat.id.toChatId(), "ازت ممنونم ${it.text}.\n" +
-				"لطفا نام و نام خانوادگی خود را به انگلیسی وارد کنید!")
+		bot.sendMessage(it.chat.id.toChatId(), "لطفا نام و نام خانوادگی خود را به انگلیسی وارد کنید!")
 	}.then {
 		nameEn ="${it.text}"
+
 		bot.sendMessage(it.chat.id.toChatId(), "نام فارسی : $nameFa\n" +
 				"نام انگلیسی : $nameEn\n" +
 				"آیا اطلاعات وارد شده درست است ؟", replyMarkup = ReplyKeyboardMarkup( keyboard = listOf(
@@ -71,7 +82,8 @@ fun main(args: Array<String>)
 				KeyboardButton("ساختن تصویر نوشته جدید!")
 			)
 		),
-		oneTimeKeyboard = true)).let { msg ->
+		oneTimeKeyboard = true)
+		).let { msg ->
 			msgID = msg.messageId
 		}
 	}.then {
@@ -93,7 +105,60 @@ fun main(args: Array<String>)
 				bot.deleteMessage(it.chat.id.toChatId(), msgID)
 				bot.jumpToAndFire("run", it)
 			}
-			"ساختن تصویر نوشته جدید!" -> bot.sendMessage(it.chat.id.toChatId(), "ساحته شد !")
+			"ساختن تصویر نوشته جدید!" ->
+			{
+				bot.deleteMessage(it.chat.id.toChatId(), msgID)
+				bot.sendMessage(it.chat.id.toChatId(), "در حال ساختن ...")
+
+				val file = ResourceUtils.getFile("classpath:temp.jpeg")
+				val bos = ByteArrayOutputStream()
+				try {
+					run {
+						val nameFaFont = Font.createFont(Font.TRUETYPE_FONT, ResourceUtils.getFile("classpath:Dana-Black.ttf")).deriveFont(30.0f)
+						val nameEnFont = Font.createFont(Font.TRUETYPE_FONT, ResourceUtils.getFile("classpath:Gotham-Book.otf")).deriveFont(12.0f)
+						val g = ImageIO.read(file)
+
+						val url = URL("https://api.telegram.org/file/bot$token/${bot.getFile(bot.getUserProfilePhotos(it.chat.id).photos[0][0].fileId).filePath!!}")
+
+						val profilePhoto = ImageIO.read(url)
+
+						val output = BufferedImage(182, 182, BufferedImage.TYPE_INT_ARGB)
+						val g2 = output.createGraphics()
+						g2.composite = AlphaComposite.Src
+						g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+						g2.color = Color.WHITE
+						g2.fill(RoundRectangle2D.Float(0f, 0f, 182f, 182f, 360f, 360f))
+
+						g2.composite = AlphaComposite.SrcAtop
+						g2.drawImage(profilePhoto, 0, 0, 182, 182, null)
+						g2.dispose()
+
+						g.graphics.drawImage(output,136, 263,182, 182, null)
+
+
+						var attributedText = AttributedString(nameFa)
+						attributedText.addAttribute(TextAttribute.FONT, nameFaFont)
+						attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE)
+						var metrics: FontMetrics = g.graphics.getFontMetrics(nameFaFont)
+						var positionX: Int = (g.width - metrics.stringWidth(nameFa)) / 2
+						g.graphics.drawString(attributedText.iterator, positionX, 490)
+
+						attributedText = AttributedString(nameEn)
+						attributedText.addAttribute(TextAttribute.FONT, nameEnFont)
+						attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE)
+						metrics = g.graphics.getFontMetrics(nameEnFont)
+						positionX = (g.width - metrics.stringWidth(nameEn)) / 2
+						g.graphics.drawString(attributedText.iterator, positionX, 520)
+
+
+						g.graphics.dispose()
+						ImageIO.write(g, "jpeg", bos)
+					}
+				}
+				finally {
+					bot.sendPhoto(it.chat.id.toChatId(), SendingByteArray(bos.toByteArray(), "${nameEn}.jpeg"))
+				}
+			}
 		}
 	}.build()
 
