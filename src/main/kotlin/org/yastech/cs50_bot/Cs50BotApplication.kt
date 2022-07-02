4e8ccbaabf3e22b975c0ab9e93b23a36e98c2f1e
Hossein Araghi <hoseinaraghi84@gmail.com>
2022-07-03 02:50:02 +0430

    FIX: Project Comments

--- src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
+++ src/main/kotlin/org/yastech/cs50_bot/Cs50BotApplication.kt
@@ -25,6 +25,7 @@ class Cs50BotApplication
 
 fun main(args: Array<String>)
 {
+	// Run Spring Boot Application
 	runApplication<Cs50BotApplication>(*args)
 
 	// Set Bot Token
@@ -57,20 +58,33 @@ fun main(args: Array<String>)
 	}
 
 	// Generate Command
+
+	// Global Variables
 	var nameFa = ""
 	var nameEn = ""
 	var msgID = 0L
+
+	// Command Chain
 	bot.chain("/generate")
 	{
+
+		// Run Process
 		bot.jumpToAndFire("run", it)
 	}.then("run"){
+
+		// Get FullName In Persian
 		bot.sendMessage(it.chat.id.toChatId(), "لطفا نام و نام خانوادگی خود را به فارسی وارد کنید!")
 	}.then{
+
+		// Set User Response And Get User FullName In English
 		nameFa = "${it.text}"
 		bot.sendMessage(it.chat.id.toChatId(), "لطفا نام و نام خانوادگی خود را به انگلیسی وارد کنید!")
 	}.then {
+
+		// Set User Response
 		nameEn ="${it.text}"
 
+		// Get Data Promise From User
 		bot.sendMessage(it.chat.id.toChatId(), "نام فارسی : $nameFa\n" +
 				"نام انگلیسی : $nameEn\n" +
 				"آیا اطلاعات وارد شده درست است ؟", replyMarkup = ReplyKeyboardMarkup( keyboard = listOf(
@@ -82,16 +96,26 @@ fun main(args: Array<String>)
 				KeyboardButton("ساختن تصویر نوشته جدید!")
 			)
 		),
+
+		// Close After Click
 		oneTimeKeyboard = true)
 		).let { msg ->
+
+			// Set Promise Message ID
 			msgID = msg.messageId
 		}
 	}.then {
+
+		// Check User Response
 		when(it.text)
 		{
+			// On Cancel
 			"لغو" ->
 			{
+				// Remove Keyboard
 				bot.deleteMessage(it.chat.id.toChatId(), msgID)
+
+				// Show Cancel Response
 				bot.sendMessage(it.chat.id.toChatId(), "عملیات با موفقیت لغو شد!")
 				bot.sendMessage(it.chat.id.toChatId(), "دستورات ربات :\n" +
 						"\n" +
@@ -101,65 +125,122 @@ fun main(args: Array<String>)
 						"\n" +
 						"برای پشتیبانی بات با این ایمیل در تماس باشید : yastechorg@gmail.com")
 			}
+
+			// On Edit Data
 			"ویرایش اطلاعات" -> {
+
+				// Remove Keyboard
 				bot.deleteMessage(it.chat.id.toChatId(), msgID)
+
+				// Restart Process
 				bot.jumpToAndFire("run", it)
 			}
+
+			// On Create New Image
 			"ساختن تصویر نوشته جدید!" ->
 			{
+
+				// Remove Keyboard
 				bot.deleteMessage(it.chat.id.toChatId(), msgID)
+
+				// Send Waiting Response
 				bot.sendMessage(it.chat.id.toChatId(), "در حال ساختن ...")
 
+				// Get Temp Image From Application Resources And Initialize It
 				val file = ResourceUtils.getFile("classpath:temp.jpeg")
+
+				// Initialize Final Byte Array Output Variable
 				val bos = ByteArrayOutputStream()
+
+				// Design Process
 				try {
+
+					// Do Process In Blocking Context
 					run {
+
+						// Read Fonts From Application Resources And Create Custom Fonts
 						val nameFaFont = Font.createFont(Font.TRUETYPE_FONT, ResourceUtils.getFile("classpath:Dana-Black.ttf")).deriveFont(30.0f)
 						val nameEnFont = Font.createFont(Font.TRUETYPE_FONT, ResourceUtils.getFile("classpath:Gotham-Book.otf")).deriveFont(12.0f)
+
+						// Main Canvas
 						val g = ImageIO.read(file)
 
-						val url = URL("https://api.telegram.org/file/bot$token/${bot.getFile(bot.getUserProfilePhotos(it.chat.id).photos[0][0].fileId).filePath!!}")
+						// Load User Profile Photo
+						val profilePhoto = ImageIO.read(
+							URL("https://api.telegram.org/file/bot$token/${bot.getFile(bot.getUserProfilePhotos(it.chat.id).photos[0][0].fileId).filePath!!}")
+						)
 
-						val profilePhoto = ImageIO.read(url)
+						// Create User Profile Photo Buffer Image
+						val profilePhotoOutput = BufferedImage(182, 182, BufferedImage.TYPE_INT_ARGB)
 
-						val output = BufferedImage(182, 182, BufferedImage.TYPE_INT_ARGB)
-						val g2 = output.createGraphics()
+						// Create User Profile Photo Canvas
+						val g2 = profilePhotoOutput.createGraphics()
+
+						// Draw Base Circle
 						g2.composite = AlphaComposite.Src
 						g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
 						g2.color = Color.WHITE
 						g2.fill(RoundRectangle2D.Float(0f, 0f, 182f, 182f, 360f, 360f))
-
 						g2.composite = AlphaComposite.SrcAtop
+
+						// Draw Profile Photo In Canvas
 						g2.drawImage(profilePhoto, 0, 0, 182, 182, null)
+
+						// Render Profile Photo Result
 						g2.dispose()
 
-						g.graphics.drawImage(output,136, 263,182, 182, null)
+						// Draw Profile Photo In Canvas
+						g.graphics.drawImage(profilePhotoOutput,136, 263,182, 182, null)
 
-
+						// Create New Attributed String And Pass Persian Name String
 						var attributedText = AttributedString(nameFa)
+
+						// Set Persian Name Font Style
 						attributedText.addAttribute(TextAttribute.FONT, nameFaFont)
 						attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE)
+
+						// Get Persian Name Font Metrics
 						var metrics: FontMetrics = g.graphics.getFontMetrics(nameFaFont)
+
+						// Get Persian Name Center Position From Font Metrics
 						var positionX: Int = (g.width - metrics.stringWidth(nameFa)) / 2
+
+						// Draw Persian Name With ( x : center , y : 490px )
 						g.graphics.drawString(attributedText.iterator, positionX, 490)
 
+
+						// Reinitialize Attributed String And Pass English Name String
 						attributedText = AttributedString(nameEn)
+
+						// Set English Name Font Style
 						attributedText.addAttribute(TextAttribute.FONT, nameEnFont)
 						attributedText.addAttribute(TextAttribute.FOREGROUND, Color.WHITE)
+
+						// Get English Name Font Metrics
 						metrics = g.graphics.getFontMetrics(nameEnFont)
+
+						// Get English Name Center Position From Font Metrics
 						positionX = (g.width - metrics.stringWidth(nameEn)) / 2
+
+						// Draw English Name With ( x : center , y : 520px )
 						g.graphics.drawString(attributedText.iterator, positionX, 520)
 
-
+						// Render Main Canvas
 						g.graphics.dispose()
+
+						// Write Final Rendered Result On Byte Array Output Stream On JPEG Format
 						ImageIO.write(g, "jpeg", bos)
 					}
 				}
 				finally {
+
+					// Finally Send Result Photo As ByteArray To User
 					bot.sendPhoto(it.chat.id.toChatId(), SendingByteArray(bos.toByteArray(), "${nameEn}.jpeg"))
 				}
 			}
 		}
+
+	// Build Generate Chain
 	}.build()
 
 	// Start Bot
